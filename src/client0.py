f4a1abd667aed16b6abc3d202a91f6579362b13b
zafaryabZhcet <96406318+zafaryabZhcet@users.noreply.github.com>
2024-05-22 12:34:04 -0400

    clients

--- src/client0.py
+++ src/client0.py
@@ -29,7 +29,7 @@ model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
 # Load dataset
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 x_train, x_test = x_train/255.0, x_test/255.0
-dist = [0, 0, 1000, 0, 0, 0, 0, 0, 0, 0]
+dist = [1000, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 x_train, y_train = getData(dist, x_train, y_train)
 
 
@@ -39,13 +39,16 @@ class FlowerClient(fl.client.NumPyClient):
         return model.get_weights()
 
     def fit(self, parameters, config):
+        
         model.set_weights(parameters)
         print("Aggregated Weights: ",model.get_weights()[-1])
-        r = model.fit(x_train, y_train, epochs=2, validation_data=(x_test, y_test), verbose=0)
+        
+        r = model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test), verbose=0)
         print("Weights: ",model.get_weights()[-1])
-
         hist = r.history
         print("Fit history : " ,hist)
+
+        print(model)
         return model.get_weights(), len(x_train), {}
 
     def evaluate(self, parameters, config):
@@ -53,5 +56,6 @@ class FlowerClient(fl.client.NumPyClient):
         loss, accuracy = model.evaluate(x_test, y_test, verbose=0)
         print("Eval accuracy : ", accuracy)
         return loss, len(x_test), {"accuracy": accuracy}
+
 
 fl.client.start_client(server_address="[::]:8080", client=FlowerClient().to_client())
--- src/client5.py
+++ src/client5.py
@@ -29,7 +29,7 @@ model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
 # Load dataset
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 x_train, x_test = x_train/255.0, x_test/255.0
-dist = [0, 0, 1000, 0, 0, 0, 0, 0, 0, 0]
+dist = [0, 0, 0, 0, 0, 1000, 0, 0, 0, 0]
 x_train, y_train = getData(dist, x_train, y_train)
 
 
