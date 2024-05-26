9b32ad8e0a57eb3d01f7c08e41b0620d89c2d2d4
zafaryabZhcet <96406318+zafaryabZhcet@users.noreply.github.com>
2024-05-26 13:36:08 -0400

    agg evaluation at server

--- src/server.py
+++ src/server.py
@@ -2,19 +2,24 @@ import flwr as fl
 import numpy as np
 import csv
 
+from utils import get_dataset,load_model
+import os 
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+# Load and compile Keras model
+model = load_model()
+x_test, y_test = np.load('./X_test.npy'), np.load('./Y_test.npy')
+
+
 class SaveModelStrategy(fl.server.strategy.FedAvg):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, min_fit_clients=30, min_available_clients=30, min_evaluate_clients=30, *args, **kwargs):
+        super().__init__(min_fit_clients=min_fit_clients,
+                         min_available_clients=min_available_clients,
+                         min_evaluate_clients=min_evaluate_clients,
+                         *args, **kwargs)
         self.client_weights = {}
 
-    def aggregate_fit(
-        self,
-        rnd,
-        results,
-        failures
-    ):
-        
-
+    def aggregate_fit(self,rnd,results,failures):
         # Perform aggregation using the superclass method
         aggregated_weights = super().aggregate_fit(rnd, results, failures)
     
@@ -36,6 +41,12 @@ class SaveModelStrategy(fl.server.strategy.FedAvg):
             file.write(str(rnd))
         file.close()
 
+        agg_weights = fl.common.parameters_to_ndarrays(aggregated_weights[0])
+        model.set_weights(agg_weights)
+        loss, accuracy = model.evaluate(x_test, y_test, verbose=0)
+
+        print(f"After {rnd} Round: loss = {loss}, accuracy: {accuracy}")
+
         return aggregated_weights
     
     def get_classification_number(self, client_name):
@@ -48,10 +59,72 @@ strategy = SaveModelStrategy()
 
 fl.server.start_server(
     server_address="localhost:8080",
-    config=fl.server.ServerConfig(num_rounds=15), 
+    config=fl.server.ServerConfig(num_rounds=10), 
     strategy=strategy
     )
 
 with open("rnd.txt", 'w') as file:
     file.write(str(0))
 file.close()
+
+
+
+#######OLD method where each client was evaluating##############
+# import flwr as fl
+
+# import numpy as np
+# import csv
+
+# class SaveModelStrategy(fl.server.strategy.FedAvg):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.client_weights = {}
+
+#     def aggregate_fit(
+#         self,
+#         rnd,
+#         results,
+#         failures
+#     ):
+        
+
+#         # Perform aggregation using the superclass method
+#         aggregated_weights = super().aggregate_fit(rnd, results, failures)
+    
+#         # Access and save weights from individual clients
+#         for client_res in results:
+#             client_id = client_res[1].metrics["client_name"]
+#             weights = client_res[1].parameters
+#             # np.savez(f"./weights/round-{rnd}-weights-{client_id}.npz", weights)
+#             client_weights_list = fl.common.parameters_to_ndarrays(weights)
+            
+#             # Convert numpy arrays to lists
+#             client_weights_as_lists = [w.tolist() for w in client_weights_list]
+            
+#             # Save client weights as lists
+#             print(f"Saving round {rnd} client {client_id} weights as lists...")
+#             np.savez(f"./weights/round-{rnd}-client-{client_id}.npz", *client_weights_as_lists)
+
+#         with open("rnd.txt", 'w') as file:
+#             file.write(str(rnd))
+#         file.close()
+
+#         return aggregated_weights
+    
+#     def get_classification_number(self, client_name):
+#         # Derive classification number from client name
+#         return int(client_name.replace('client', ''))
+    
+
+# # Create strategy and run server
+# strategy = SaveModelStrategy()
+
+# fl.server.start_server(
+#     server_address="localhost:8080",
+#     config=fl.server.ServerConfig(num_rounds=10), 
+#     strategy=strategy
+#     )
+
+# with open("rnd.txt", 'w') as file:
+#     file.write(str(0))
+# file.close()
--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,10 @@ from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers import Dense, Conv1D, MaxPooling1D, Flatten, Dropout, BatchNormalization
+from tensorflow.keras.optimizers import Adam
+
+import numpy as np
 
 def load_raw_data(df, sampling_rate, path):
     if sampling_rate == 100:
@@ -22,37 +26,106 @@ def load_raw_data(df, sampling_rate, path):
 def get_dataset(target=0, client=0):
 
     path='./ECG_data/'
-    path_csv = './filtered_data.csv'
+    path_csv = './federated_data.csv'
     sampling_rate=100
-    sample_size = 300
+    sample_size = 200
   
     # load and convert annotation data
     Y = pd.read_csv(path_csv)
     Y_data = Y[Y['diagnostic_superclass']==target]
     if target==1:
-        client -= 20
-        sample_size = 350
+        client = client- 20
+        sample_size = 200
     start = client*sample_size
     Y_data = Y_data.iloc[start:start+sample_size]
     Y = Y_data['diagnostic_superclass'].values
-
     # Load raw signal data
     X = load_raw_data(Y_data, sampling_rate, path)
-    Y = tf.keras.utils.to_categorical(Y,2)
-    x_test, y_test = np.load('./X_test.npy'), np.load('./Y_test.npy')
-
-    return X, Y,  x_test, y_test
+    
+    return X, Y
 
 def load_model():
-    model = Sequential()
-    model.add(LSTM(50, activation='relu',return_sequences=True, input_shape=(1000,12)))
-    model.add(LSTM(50, activation='relu', input_shape=(1000,12)))
-    model.add(Dropout(0.2))
-    model.add(Dense(2, activation='softmax'))
-    # Compile the model
-    model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+    model = Sequential([
+    Conv1D(filters=32, kernel_size=5, activation='relu', input_shape=(1000, 12)),
+    MaxPooling1D(pool_size=2),
+    BatchNormalization(),
+    Conv1D(filters=64, kernel_size=5, activation='relu'),
+    MaxPooling1D(pool_size=2),
+    BatchNormalization(),
+    Conv1D(filters=128, kernel_size=5, activation='relu'),
+    MaxPooling1D(pool_size=2),
+    BatchNormalization(),
+    Flatten(),
+    Dropout(0.5),
+    Dense(256, activation='relu'),
+    Dropout(0.5),
+    Dense(1, activation='sigmoid')  # For binary classification
+])
+    model.compile(optimizer=Adam(learning_rate=0.001),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
 
     return model
+
+
+
+
+#######OLD method where each client was evaluating##############
+# import os
+# import numpy as np
+# import tensorflow as tf
+# import pandas as pd
+# import numpy as np
+# import wfdb
+# from tensorflow import keras
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import LSTM, Dense, Dropout
+# from sklearn.model_selection import train_test_split
+
+# def load_raw_data(df, sampling_rate, path):
+#     if sampling_rate == 100:
+#         data = [wfdb.rdsamp(path+f) for f in df.filename_lr]
+#     else:
+#         data = [wfdb.rdsamp(path+f) for f in df.filename_hr]
+    
+#     data = np.array([signal for signal, meta in data])
+#     return data
+
+
+# def get_dataset(target=0, client=0):
+
+#     path='./ECG_data/'
+#     path_csv = './filtered_data.csv'
+#     sampling_rate=100
+#     sample_size = 300
+  
+#     # load and convert annotation data
+#     Y = pd.read_csv(path_csv)
+#     Y_data = Y[Y['diagnostic_superclass']==target]
+#     if target==1:
+#         client -= 20
+#         sample_size = 350
+#     start = client*sample_size
+#     Y_data = Y_data.iloc[start:start+sample_size]
+#     Y = Y_data['diagnostic_superclass'].values
+
+#     # Load raw signal data
+#     X = load_raw_data(Y_data, sampling_rate, path)
+#     Y = tf.keras.utils.to_categorical(Y,2)
+#     x_test, y_test = np.load('./X_test.npy'), np.load('./Y_test.npy')
+
+#     return X, Y,  x_test, y_test
+
+# def load_model():
+#     model = Sequential()
+#     model.add(LSTM(50, activation='relu',return_sequences=True, input_shape=(1000,12)))
+#     model.add(LSTM(50, activation='relu', input_shape=(1000,12)))
+#     model.add(Dropout(0.2))
+#     model.add(Dense(2, activation='softmax'))
+#     # Compile the model
+#     model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+
+#     return model
 
 
 
