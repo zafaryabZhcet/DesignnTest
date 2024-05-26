a7e7151c670e6242bc816e03bac511370633ada5
zafaryabZhcet <96406318+zafaryabZhcet@users.noreply.github.com>
2024-05-26 16:26:23 -0400

    10 rnd 15 epochs

--- src/server.py
+++ src/server.py
@@ -73,7 +73,7 @@ strategy = SaveModelStrategy()
 
 fl.server.start_server(
     server_address="localhost:8080",
-    config=fl.server.ServerConfig(num_rounds=3), 
+    config=fl.server.ServerConfig(num_rounds=10), 
     strategy=strategy
     )
 
